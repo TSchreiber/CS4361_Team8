9618534b5cc1b5fca885786c097dc5ab77c2ff57
TSchreiber <24798607+TSchreiber@users.noreply.github.com>
2022-11-09 22:14:38 -0600

    feat: timer is now pausable

--- app/src/main/java/t8/Timer.java
+++ app/src/main/java/t8/Timer.java
@@ -8,8 +8,10 @@ public class Timer {
 
     private long time;
     private long startTime;
+    private long offset;
     private SimpleStringProperty sspTime;
     boolean running = false;
+    boolean paused = false;
 
     public Timer() {
         sspTime = new SimpleStringProperty("00:00:000");
@@ -17,8 +19,10 @@ public class Timer {
 
     public void startTimer() {
         this.time = System.currentTimeMillis();
+        this.offset = 0;
         this.startTime = this.time;
         running = true;
+        setPaused(false);
         new Thread( () -> {
             while (running) {
                 try {
@@ -28,7 +32,9 @@ public class Timer {
                 catch (Exception ex) {
                     ex.printStackTrace();
                 }
-                updateTime();
+                if (!isPaused()) {
+                    updateTime();
+                }
             }
         }).start();
     }
@@ -37,10 +43,34 @@ public class Timer {
         running = false;
     }
 
+    public boolean isPaused() {
+        return paused;
+    }
+
+    public void setPaused(boolean paused) {
+        // going from unpaused to paused
+        if (!isPaused() && paused) {
+            this.offset += System.currentTimeMillis() - startTime;
+        } 
+        // going from paused to unpaused
+        else if (isPaused() && !paused) {
+            this.startTime = System.currentTimeMillis();
+        }
+        this.paused = paused;
+    }
+
+    public void pause() {
+        setPaused(true);
+    }
+
+    public void unpause() {
+        setPaused(false);
+    }
+
     public void updateTime() {
         if (Platform.isFxApplicationThread()) {
             this.time = System.currentTimeMillis();
-            long dt = time - startTime;
+            long dt = time - startTime + offset;
             sspTime.set(String.format("%02d:%02d:%03d", dt/60_000, (dt / 1000) % 60, dt % 1000));
         } else {
             Platform.runLater(() -> updateTime());
@@ -49,6 +79,9 @@ public class Timer {
 
     public void resetTimer() {
         this.startTime = System.currentTimeMillis();
+        this.offset = 0;
+        this.running = true;
+        this.paused = false;
     }
 
     public long getTime() {
