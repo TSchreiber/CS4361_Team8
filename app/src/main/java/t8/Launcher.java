e299883cacd4e62ab18bd3fe062d35bdd538a1fe
TSchreiber <24798607+TSchreiber@users.noreply.github.com>
2022-11-09 22:14:55 -0600

    feat: pause button

--- app/src/main/java/t8/Launcher.java
+++ app/src/main/java/t8/Launcher.java
@@ -1,6 +1,8 @@
 package t8;
 
 import javafx.application.Application;
+import javafx.beans.*;
+import javafx.application.*;
 import javafx.event.*;
 import javafx.geometry.*;
 import javafx.scene.*;
@@ -11,27 +13,69 @@ import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.*;
 import javafx.scene.shape.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.scene.transform.*;
 import javafx.stage.*;
 import javafx.fxml.*;
 import java.util.*;
+import java.util.Observable;
 import java.net.*;
 import com.interactivemesh.jfx.importer.obj.ObjModelImporter;
 import com.interactivemesh.jfx.importer.*;
 
 public class Launcher extends Application {
+    
+	public final static Button buttonRestart = new Button("Restart");
+    final Button buttonClose = new Button("Close");
+    final Button buttonPause = new Button("Pause");
+    private Font font = new Font("Verdana", 36);
+	public static Integer i = 0;
 
+    
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        VBox root = new VBox();
-        Scene scene = new Scene(root, Color.BLANCHEDALMOND);
+        HBox root = new HBox();
 
-        HBox menu = new HBox();
-        menu.getChildren().addAll(new Button("Button1"), new Button("Button2"));
+        Text moves = new Text("MOVES");
+        moves.setFont(Font.font("Helvetica", FontWeight.BOLD, 70));
+        Label moveCount = new Label(i.toString());
+        Timer timer = new Timer();
+        timer.startTimer();
+        stage.setOnCloseRequest(e -> timer.stopTimer());
+        Text time = new Text("TIME");
+        time.setFont(Font.font("Helvetica", FontWeight.BOLD, 70));
+        Label timeCount = new Label();
+        timeCount.textProperty().bind(timer.getSspTime());
+       
+        moveCount.setAlignment(Pos.CENTER);
+        moveCount.setMaxWidth(200);
+        timeCount.setAlignment(Pos.CENTER);
+        timeCount.setMaxWidth(200);
+        moveCount.setFont(Font.font("Helvetica", FontWeight.BOLD, 50));
+        timeCount.setFont(Font.font("Helvetica", FontWeight.BOLD, 30));
+        buttonRestart.setPrefSize(200, 50);
+        buttonClose.setPrefSize(200, 50);
+        buttonRestart.setFont(Font.font("Helvetica", FontWeight.BOLD, 30));
+        buttonClose.setFont(Font.font("Helvetica", FontWeight.BOLD, 30));
+        buttonPause.setPrefSize(200, 50);
+        buttonPause.setFont(Font.font("Helvetica", FontWeight.BOLD, 30));
+
+        VBox sidebar = new VBox(moves, moveCount,time,timeCount, buttonRestart, buttonPause, buttonClose);
+        sidebar.setSpacing(25);
+        sidebar.setAlignment(Pos.TOP_CENTER);
+        Scene scene = new Scene(root, Color.BLANCHEDALMOND);
+        root.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
+
+        sidebar.setBackground(new Background(new BackgroundFill(Color.BLANCHEDALMOND, null, null)));
+        sidebar.setBorder(new Border(new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(3))));
+        root.setBorder(new Border(new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(3))));
+        sidebar.setPadding(new Insets(25));
 
         Group root3d = new Group();
         SubScene scene3d = new SubScene(root3d, 1200, 800, true, SceneAntialiasing.BALANCED);
@@ -43,6 +87,61 @@ public class Launcher extends Application {
         root3d.setScaleZ(50);
         root3d.setTranslateX(600);
         root3d.setTranslateY(400);
+        
+        buttonRestart.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent a) {
+				try {
+					i = 0;
+					moveCount.setText(i.toString());
+                    timer.resetTimer();
+					root3d.getChildren().clear();
+					var cube = new RubiksCube(); 
+					root3d.getChildren().addAll(cube);
+					root3d.getChildren().add(new AmbientLight());
+                    MouseHandler mouseHandler = new MouseHandler(cube);
+                    scene3d.setOnMousePressed(mouseHandler);
+                    scene3d.setOnMouseDragged(mouseHandler);
+                    scene3d.setOnMouseReleased(mouseHandler);
+                    if(buttonPause.getText() == "Resume") {
+						buttonPause.setText("Pause");
+					}
+				} catch (Exception el) {
+					el.printStackTrace();
+				}
+			}
+		});
+        
+        buttonPause.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent a) {
+				try {
+					if(buttonPause.getText() == "Pause") {
+                        timer.pause();
+						buttonPause.setText("Resume");
+					}
+					else {
+                        timer.unpause();
+						buttonPause.setText("Pause");
+					}
+				} catch (Exception el) {
+					el.printStackTrace();
+				}
+				
+			}
+        });
+        
+        buttonClose.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent a) {
+				try {
+					stage.close();
+                    stage.getOnCloseRequest().handle(null);
+				} catch (Exception el) {
+					el.printStackTrace();
+				}
+			}
+		});
 
         var cube = new RubiksCube();
         root3d.getChildren().addAll(cube);
@@ -52,13 +151,12 @@ public class Launcher extends Application {
         scene3d.setOnMouseDragged(mouseHandler);
         scene3d.setOnMouseReleased(mouseHandler);
 
-        root.getChildren().addAll(menu, scene3d);
+        root.getChildren().addAll(scene3d, sidebar);
+        
 
         stage.setTitle("Rubik's Cube");
         stage.setScene(scene);
         stage.show();
     }
-
-    
 
 }
