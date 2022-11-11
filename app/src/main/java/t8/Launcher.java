312b7e06c6bacb0d73b35d2702b2e1c85dd4a33c
TSchreiber <24798607+TSchreiber@users.noreply.github.com>
2022-11-10 18:35:29 -0600

    Priyesh's leaderboard code

--- app/src/main/java/t8/Launcher.java
+++ app/src/main/java/t8/Launcher.java
@@ -42,6 +42,7 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         HBox root = new HBox();
+        Scene scene = new Scene(root, Color.BLANCHEDALMOND);
 
         Text moves = new Text("MOVES");
         moves.setFont(Font.font("Helvetica", FontWeight.BOLD, 70));
@@ -67,10 +68,34 @@ public class Launcher extends Application {
         buttonPause.setPrefSize(200, 50);
         buttonPause.setFont(Font.font("Helvetica", FontWeight.BOLD, 30));
 
-        VBox sidebar = new VBox(moves, moveCount,time,timeCount, buttonRestart, buttonPause, buttonClose);
+        Leaderboard L = new Leaderboard();
+        /**********************************************************************************
+                NEED TO UPDATE MOVE, TIME, AND SCORE WHEN THE CUBE IS COMPLETED
+        ***********************************************************************************/
+        Score newPlayerScore = new Score("", 0, "00:00", 0);
+        TextField playerTextField = new TextField("Enter Name!");
+
+        Button leaderboardButton = new Button("Leaderboard");
+
+        Button returnButton = new Button("Back to game");
+        returnButton.setOnAction(e -> stage.setScene(scene));
+
+        Button saveButton = new Button("Save Score");
+
+        leaderboardButton.setOnAction(e -> {
+            displayLeaderboard(stage, returnButton, saveButton, playerTextField, true, newPlayerScore);
+        });
+
+        saveButton.setOnAction(e ->{
+            newPlayerScore.name = playerTextField.getText();
+            L.addScoreToList(newPlayerScore);
+            displayLeaderboard(stage, returnButton, saveButton, playerTextField, false, newPlayerScore);
+        });
+
+        VBox sidebar = new VBox(moves, moveCount,time,timeCount, buttonRestart, 
+            buttonPause, leaderboardButton, buttonClose);
         sidebar.setSpacing(25);
         sidebar.setAlignment(Pos.TOP_CENTER);
-        Scene scene = new Scene(root, Color.BLANCHEDALMOND);
         root.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
 
         sidebar.setBackground(new Background(new BackgroundFill(Color.BLANCHEDALMOND, null, null)));
@@ -183,5 +208,111 @@ public class Launcher extends Application {
     private int getScore() {
         return (int)(timer.getTime() * i);
     }
+
+    public void displayLeaderboard (Stage mainStage, Button returnButton, Button saveScore, TextField playerTextField, Boolean newScore, Score newPlayerScore){
+        VBox leaderboardVBox = new VBox(30);
+        Scene leaderboardScene = new Scene(leaderboardVBox, 1200, 800,Color.BLANCHEDALMOND);
+        leaderboardVBox.setAlignment(Pos.CENTER);
+
+        //clear leaderboard scene to display lastest scores
+        leaderboardVBox.getChildren().clear();
+
+        mainStage.setScene(leaderboardScene);
+
+        //Leaderboard Label
+        Label leaderboardLabel = new Label("LEADERBOARD!");
+        leaderboardLabel.setFont(new Font("Arial", 72));
+
+        //Feature elements
+        HBox featureBox = new HBox(75);
+        featureBox.setAlignment(Pos.CENTER);
+
+        Label featureRankLabel = new Label("Rank");
+        featureRankLabel.setFont(new Font("Arial", 32));
+        featureRankLabel.setUnderline(true);
+
+        Label featureNameLabel = new Label("Name");
+        featureNameLabel.setFont(new Font("Arial", 32));
+        featureNameLabel.setUnderline(true);
+
+        Label featureMoveLabel = new Label("Moves");
+        featureMoveLabel.setFont(new Font("Arial", 32));
+        featureMoveLabel.setUnderline(true);
+
+        Label featureTimeLabel = new Label("Time");
+        featureTimeLabel.setFont(new Font("Arial", 32));
+        featureTimeLabel.setUnderline(true);
+
+        Label featureTotalScoreLabel = new Label("Score");
+        featureTotalScoreLabel.setFont(new Font("Arial", 32));
+        featureTotalScoreLabel.setUnderline(true);
+
+
+        featureBox.setAlignment(Pos.CENTER);
+        featureBox.getChildren().addAll(featureRankLabel,featureNameLabel,featureMoveLabel,featureTimeLabel,featureTotalScoreLabel);
+        leaderboardVBox.getChildren().addAll(leaderboardLabel,featureBox);
+
+        Leaderboard L = new Leaderboard();
+        int rank = 1;
+        Collections.sort(L.ScoreList);
+        for (int i = 0; i < L.ScoreList.size() && i < 5; i++) {
+            //User Elements
+            HBox userHBox = new HBox(125);
+            userHBox.setAlignment(Pos.CENTER);
+            userHBox.setMaxWidth(1000);
+
+            Label userRankLabel = new Label(String.valueOf(rank));
+            userRankLabel.setFont(new Font("Arial", 28));
+
+            Label userNameLabel = new Label(L.ScoreList.get(i).name);
+            userNameLabel.setFont(new Font("Arial", 28));
+
+            Label userMoveLabel = new Label(String.valueOf(L.ScoreList.get(i).moveCount));
+            userMoveLabel.setFont(new Font("Arial", 28));
+
+            Label userTimeLabel = new Label(L.ScoreList.get(i).timeCount);
+            userTimeLabel.setFont(new Font("Arial", 28));
+
+            Label userTotalScoreLabel = new Label(String.valueOf(L.ScoreList.get(i).totalScore));
+            userTotalScoreLabel.setFont(new Font("Arial", 28));
+
+            userHBox.getChildren().addAll(userRankLabel,userNameLabel,userMoveLabel,userTimeLabel,userTotalScoreLabel);
+            Label scoreLabel = new Label(rank + ". "+ L.ScoreList.get(i).toString());
+            scoreLabel.setFont(new Font("Arial", 30));
+
+            leaderboardVBox.getChildren().add(userHBox);
+            rank++;
+        }
+
+        //new score present
+        if(newScore == true){
+            HBox playerHBox = new HBox(100);
+            featureBox.setAlignment(Pos.CENTER);
+
+            Label playerRankLabel = new Label("Your Score:");
+            playerRankLabel.setFont(new Font("Arial", 32));
+
+            TextField playerNameLabel = new TextField("Enter Name!");
+            playerNameLabel.setPrefWidth(100);
+            playerNameLabel.setPrefHeight(50);
+
+            Label playerMoveLabel = new Label(String.valueOf(newPlayerScore.moveCount));
+            playerMoveLabel.setFont(new Font("Arial", 32));
+
+            Label playerTimeLabel = new Label(newPlayerScore.timeCount);
+            playerTimeLabel.setFont(new Font("Arial", 32));
+
+            Label playerTotalScoreLabel = new Label(String.valueOf(newPlayerScore.totalScore));
+            playerTotalScoreLabel.setFont(new Font("Arial", 32));
+
+            playerHBox.setAlignment(Pos.CENTER);
+            playerHBox.getChildren().addAll(playerRankLabel,playerTextField,playerMoveLabel,playerTimeLabel,playerTotalScoreLabel,saveScore);
+            playerHBox.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" +
+                              "-fx-border-width: 2;" + "-fx-border-insets: 5;" +
+                             "-fx-border-radius: 5;" + "-fx-border-color: blue;");
+            leaderboardVBox.getChildren().add(playerHBox);
+        }
+        leaderboardVBox.getChildren().add(returnButton);
+    } //display method ends
 
 }
